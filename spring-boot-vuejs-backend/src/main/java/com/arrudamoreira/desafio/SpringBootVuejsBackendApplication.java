83d729095877c542881436745259154594de950d
Mario Luiz <marioluiz.arruda@gmail.com>
2020-08-07 19:07:16 -0400

    Criacao do pacote back-end

---
+++ spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/SpringBootVuejsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arrudamoreira.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVuejsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVuejsBackendApplication.class, args);
+	}
+
+}
---
+++ spring-boot-vuejs-backend/src/test/java/com/arrudamoreira/desafio/SpringBootVuejsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrudamoreira.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVuejsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
