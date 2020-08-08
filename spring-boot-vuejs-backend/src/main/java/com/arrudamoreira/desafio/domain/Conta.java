5c123ad1550c2d73f68019b5cce92a47462ce482
Mario Luiz <marioluiz.arruda@gmail.com>
2020-08-07 22:28:46 -0400

    Criacao pacote dominio e dominio de Conta

---
+++ spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/domain/Conta.java
@@ -0,0 +1,80 @@
+package com.arrudamoreira.desafio.domain;
+
+import java.io.Serializable;
+
+public class Conta implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private Integer numero;
+	private String responsavel;
+	private Double saldo;
+	
+	public Conta () {
+	}
+
+	public Conta(Integer id, Integer numero, String responsavel, Double saldo) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.responsavel = responsavel;
+		this.saldo = saldo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
