c22c1263ca375dffb73b0ed1cc64eaab596e3324
Mario Luiz <marioluiz.arruda@gmail.com>
2020-08-09 09:51:39 -0400

    Adicionado dominio Movimentacao

---
+++ spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/domain/Movimentacao.java
@@ -0,0 +1,80 @@
+package com.arrudamoreira.desafio.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Movimentacao  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private Date instante;
+	private Double valor;
+	private Integer tipo;
+	
+	public Movimentacao() {
+	}
+
+	public Movimentacao(Integer id, Date instante, Double valor) {
+		super();
+		this.id = id;
+		this.instante = instante;
+		this.valor = valor;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getInstante() {
+		return instante;
+	}
+
+	public void setInstante(Date instante) {
+		this.instante = instante;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Integer getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movimentacao other = (Movimentacao) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
