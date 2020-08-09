26b3b11594bb9a7ca0cb1288d71f2f19535fbe02
Mario Luiz <marioluiz.arruda@gmail.com>
2020-08-09 16:11:37 -0400

    Adicionado operacao de instanciacao, adicionado algumas Contas para teste BdH2

--- spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/SpringBootVuejsBackendApplication.java
+++ spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/SpringBootVuejsBackendApplication.java
@@ -1,13 +1,34 @@
 package com.arrudamoreira.desafio;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootVuejsBackendApplication {
+import com.arrudamoreira.desafio.domain.Conta;
+import com.arrudamoreira.desafio.repositories.ContaRepository;
 
+@SpringBootApplication
+public class SpringBootVuejsBackendApplication implements CommandLineRunner{
+	
+	@Autowired
+	private ContaRepository contaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootVuejsBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Conta conta1 = new Conta(null, 107597, "Luiz Carlos da Silva", 1800.00);
+		Conta conta2 = new Conta(null, 108892, "Marina Germenes Lucida", 7894.00);
+		Conta conta3 = new Conta(null, 107597, "Rafaela do Santos Barbosa", 8568.70);
+		
+		contaRepository.saveAll(Arrays.asList(conta1, conta2, conta3));
+		
 	}
 
 }
