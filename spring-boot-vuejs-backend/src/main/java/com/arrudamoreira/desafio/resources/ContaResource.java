8a28ee619b87134d8c0208297ccdeec54625842a
Mario Luiz <marioluiz.arruda@gmail.com>
2020-08-09 15:51:13 -0400

    Adicionado EndPoint find por id

--- spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/resources/ContaResource.java
+++ spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/resources/ContaResource.java
@@ -1,30 +1,25 @@
 package com.arrudamoreira.desafio.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arrudamoreira.desafio.domain.Conta;
+import com.arrudamoreira.desafio.services.ContaService;
 
 @RestController
 @RequestMapping(value = "/contas")
 public class ContaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Conta> listar() {
-		
-		Conta conta1 = new Conta(1, 107597, "Luiz Carlos da Silva", 1800.00);
-		Conta conta2 = new Conta(2, 108892, "Marina Germenes Lucida", 7894.00);
-		Conta conta3 = new Conta(1, 107597, "Rafaela do Santos Barbosa", 8568.70);
-		
-		List<Conta> contas = new ArrayList<>();
-		
-		contas.addAll(Arrays.asList(conta1,conta2,conta3));
-		
-		return contas;
+	@Autowired
+	private ContaService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Conta obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
