d451b709c73a5c46c10a530f4f9e7a204c669749
Mario Luiz <marioluiz.arruda@gmail.com>
2020-08-09 23:37:47 -0400

    Criacao da funcionalidade Criacao Conta, Saque, Deposito e Transferencia

--- spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/resources/ContaResource.java
+++ spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/resources/ContaResource.java
@@ -1,13 +1,18 @@
 package com.arrudamoreira.desafio.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.arrudamoreira.desafio.domain.Conta;
+import com.arrudamoreira.desafio.dto.ContaDTO;
 import com.arrudamoreira.desafio.services.ContaService;
 
 @RestController
@@ -21,5 +26,43 @@ public class ContaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Conta obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert (@RequestBody ContaDTO objDto) {
+		Conta obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity <Void> update(@RequestBody ContaDTO objDto, @PathVariable Integer id){
+		Conta obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/{valor}/deposito", method = RequestMethod.PUT)
+	public ResponseEntity <Void> updateDeposito(@PathVariable Integer id, @PathVariable Double valor){
+		Conta obj = service.find(id);
+		obj = service.updateMovimentacaoDeposito(obj, valor);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/{valor}/saque", method = RequestMethod.PUT)
+	public ResponseEntity <Void> updateSaque(@PathVariable Integer id, @PathVariable Double valor){
+		Conta obj = service.find(id);
+		obj = service.updateMovimentacaoSaque(obj, valor);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/{valor}/{numeroContaDestino}", method = RequestMethod.PUT)
+	public ResponseEntity <Void> updateSaque(@PathVariable Integer id, @PathVariable Double valor,@PathVariable Integer numeroContaDestino){
+		Conta obj = service.find(id);
+		obj = service.updateMovimentacaoSaque(obj, valor);
+		return ResponseEntity.noContent().build();
 	}
 }
--- spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/services/ContaService.java
+++ spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/services/ContaService.java
@@ -1,24 +1,94 @@
 package com.arrudamoreira.desafio.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arrudamoreira.desafio.domain.Conta;
+import com.arrudamoreira.desafio.domain.Movimentacao;
+import com.arrudamoreira.desafio.domain.enums.TipoMovimentacao;
+import com.arrudamoreira.desafio.dto.ContaDTO;
 import com.arrudamoreira.desafio.repositories.ContaRepository;
 import com.arrudamoreira.desafio.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class ContaService {
 
 	@Autowired
 	private ContaRepository repo;
-	
+
+	@Autowired
+	private MovimentacaoService movimentacaoService;
+
 	public Conta find(Integer id) {
 		Optional<Conta> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Conta.class.getName()));
+	}
+
+	public Conta insert(Conta obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Conta update(Conta obj) {
+		Conta newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public Conta updateMovimentacaoDeposito(Conta obj, Double valorDeposito) {
+		Optional<Conta> newObj = repo.findById(obj.getId());
+		if (valorDeposito > 0.00) {
+			newObj.get().setSaldo(newObj.get().getSaldo() + valorDeposito);
+			Movimentacao movimentacaoDeposito = new Movimentacao(null, TipoMovimentacao.DEPOSITO, valorDeposito,
+					new Date(), null, newObj.get());
+			movimentacaoService.insert(movimentacaoDeposito);
+			newObj.get().getMovimentacoes().add(movimentacaoDeposito);
+		} else {
+			return newObj.orElseThrow(() -> new ObjectNotFoundException(
+					"Valor de deposito invalido: " + valorDeposito + ", Tipo: " + Conta.class.getName()));
+		}
+		return repo.save(newObj.get());
+	}
+
+	public Conta updateMovimentacaoSaque(Conta obj, Double valorSaque) {
+		Optional<Conta> newObj = repo.findById(obj.getId());
+		if (newObj.get().getSaldo() > valorSaque) {
+			newObj.get().setSaldo(newObj.get().getSaldo() - valorSaque);
+			Movimentacao movimentacaoSaque = new Movimentacao(null, TipoMovimentacao.SAQUE, valorSaque, new Date(),
+					null, newObj.get());
+			movimentacaoService.insert(movimentacaoSaque);
+			newObj.get().getMovimentacoes().add(movimentacaoSaque);
+		} else {
+			return newObj.orElseThrow(() -> new ObjectNotFoundException(
+					"Valor de saque invalido, saldo insuficiente: " + valorSaque + ", Tipo: " + Conta.class.getName()));
+		}
+		return repo.save(newObj.get());
+	}
+
+	public Conta updateMovimentacaoTransferencia(Conta obj, Double valorDeTransferencia, Integer numeroContaDestino) {
+		Optional<Conta> newObj = repo.findById(obj.getId());
+		if (newObj.get().getSaldo() > valorDeTransferencia) {
+			newObj.get().setSaldo(newObj.get().getSaldo() - valorDeTransferencia);
+			Movimentacao movimentacaoTransferencia = new Movimentacao(null, TipoMovimentacao.TRANSFERENCIA,
+					valorDeTransferencia, new Date(), numeroContaDestino, newObj.get());
+			movimentacaoService.insert(movimentacaoTransferencia);
+			newObj.get().getMovimentacoes().add(movimentacaoTransferencia);
+		} else {
+			return newObj.orElseThrow(() -> new ObjectNotFoundException("Saldo invalido, saldo insuficiente: "
+					+ valorDeTransferencia + ", Tipo: " + Conta.class.getName()));
+		}
+		return repo.save(newObj.get());
+	}
+
+	public Conta fromDTO(ContaDTO objDto) {
+		return new Conta(objDto.getId(), objDto.getNumeroConta(), objDto.getResponsavel(), objDto.getSaldo());
+	}
+
+	private void updateData(Conta newObj, Conta obj) {
+		newObj.setResponsavel(obj.getResponsavel());
 	}
 }
