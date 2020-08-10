360a88a15364a0a8222e43874923837baf5b9cca
Mario Luiz <marioluiz.arruda@gmail.com>
2020-08-09 23:24:39 -0400

    Adicionado instanciacao de Movimentacao no BD h2 para testes

--- spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/SpringBootVuejsBackendApplication.java
+++ spring-boot-vuejs-backend/src/main/java/com/arrudamoreira/desafio/SpringBootVuejsBackendApplication.java
@@ -1,6 +1,7 @@
 package com.arrudamoreira.desafio;
 
 import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,13 +9,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.arrudamoreira.desafio.domain.Conta;
+import com.arrudamoreira.desafio.domain.Movimentacao;
+import com.arrudamoreira.desafio.domain.enums.TipoMovimentacao;
 import com.arrudamoreira.desafio.repositories.ContaRepository;
+import com.arrudamoreira.desafio.repositories.MovimentacaoRepository;
 
 @SpringBootApplication
 public class SpringBootVuejsBackendApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ContaRepository contaRepository;
+	
+	@Autowired
+	private MovimentacaoRepository movimentacaoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootVuejsBackendApplication.class, args);
@@ -28,6 +35,15 @@ public class SpringBootVuejsBackendApplication implements CommandLineRunner{
 		Conta conta3 = new Conta(null, 107597, "Rafaela do Santos Barbosa", 8568.70);
 		
 		contaRepository.saveAll(Arrays.asList(conta1, conta2, conta3));
+		
+		Movimentacao mov1 = new Movimentacao(null, TipoMovimentacao.DEPOSITO, 500.00, new Date(), null, conta1);
+		Movimentacao mov2 = new Movimentacao(null, TipoMovimentacao.SAQUE, 600.00, new Date(), null, conta1);
+		Movimentacao mov3 = new Movimentacao(null, TipoMovimentacao.DEPOSITO, 780.00, new Date(), null, conta2);
+		Movimentacao mov4 = new Movimentacao(null, TipoMovimentacao.SAQUE, 100.00, new Date(), null, conta2);
+		Movimentacao mov5 = new Movimentacao(null, TipoMovimentacao.DEPOSITO, 150.00, new Date(), null, conta3);
+		Movimentacao mov6 = new Movimentacao(null, TipoMovimentacao.SAQUE, 300.00, new Date(), null, conta3);
+		
+		movimentacaoRepository.saveAll(Arrays.asList(mov1, mov2, mov3, mov4, mov5, mov6));
 		
 	}
 
